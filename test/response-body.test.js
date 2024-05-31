1d6ad01e5cb636d3f5f5b47f0ff864fd29c33414
KahfiSmith <alkahfii2018@gmail.com>
2024-05-31 10:46:31 +0700

    belajar express: response body

---
+++ test/response-body.test.js
@@ -0,0 +1,14 @@
+import express from "express";
+import request from "supertest";
+
+const app = express();
+app.get("/", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.send(`<html><body>Hello World</body></html>`);
+});
+
+test("test response body", async () => {
+  const response = await request(app).get("/");
+  expect(response.get("Content-Type")).toContain("text/html");
+  expect(response.text).toBe("<html><body>Hello World</body></html>");
+});
